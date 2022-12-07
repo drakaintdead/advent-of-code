60d35ac2137e7f3b8a222eb06c28aa1588629bf9
Charlie Cohen <83092267+drakaintdead@users.noreply.github.com>
2022-12-06 19:00:46 -0500

    Day 1

--- 2022/1/1-2.py
+++ 2022/1/1-2.py
@@ -3,14 +3,17 @@ from itertools import groupby
 input = open("input.txt", 'r')
 inputData = input.read().splitlines()
 splitData = [list(g) for k,g in groupby(inputData, key=lambda s: s!="") if k]
+print(splitData)
 for i in range(0, len(splitData)):
     for x in range(0, len(splitData[i])):
         splitData[i][x] = int(splitData[i][x])
 
-biggestNumber = 0
+print(splitData)
+
+addedList = []
 
 for i in range(len(splitData)):
-    if sum(splitData[i]) > biggestNumber:
-        biggestNumber = sum(splitData[i])
+    addedList.append(sum(splitData[i]))
     
-print(f"Largest amount being carried by 1 elf: {biggestNumber} calories")
+topThree = sorted(addedList, reverse=True)[:3]
+print("Sum of top 3: " + str(sum(topThree))) 
